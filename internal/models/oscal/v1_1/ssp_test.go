b87fe239588ba2db88216dcf8202f6b0c1aee05b
Gustavo Carvalho <gusfcarvalho@gmail.com>
2023-08-28 07:47:00 -0300

    Adding Assessment Plan
    Signed-off-by: Gustavo Carvalho <gusfcarvalho@gmail.com>

---
+++ internal/models/oscal/v1_1/ssp_test.go
@@ -0,0 +1,37 @@
+package v1_1
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSSP(t *testing.T) {
+	a, err := os.ReadFile("testdata/ssp.json")
+	require.Nil(t, err)
+	expect := make(map[string]interface{})
+	err = json.Unmarshal(a, &expect)
+	require.Nil(t, err)
+	c := SystemSecurityPlan{}
+	err = c.FromJSON(a)
+	require.Nil(t, err)
+	b, err := c.ToJSON()
+	require.Nil(t, err)
+	got := make(map[string]interface{})
+	err = json.Unmarshal(b, &got)
+	require.Nil(t, err)
+	assert.Equal(t, expect, got)
+}
+
+func TestSSPValidate(t *testing.T) {
+	a, err := os.ReadFile("testdata/ssp.json")
+	require.Nil(t, err)
+	c := SystemSecurityPlan{}
+	err = c.FromJSON(a)
+	require.Nil(t, err)
+	err = c.Validate()
+	assert.Nil(t, err)
+}
