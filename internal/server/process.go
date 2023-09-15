1a00da3ae2f6064a85e388179be958274553fb44
James Cleveland <jc@blit.cc>
2023-09-15 12:29:17 +0100

    better logs

--- internal/server/process.go
+++ internal/server/process.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	process "github.com/compliance-framework/configuration-service/internal/models/process"
@@ -9,27 +10,33 @@ import (
 
 func (s *Server) RegisterProcess(e *echo.Echo) error {
 	e.GET("/assessment-results/:uuid", s.getAssessmentResult)
-	e.GET("/assessment-results", s.listAssessmentResults)
+	e.GET("/assessment-results", s.getAssessmentResults)
 	return nil
 }
 
 func (s *Server) getAssessmentResult(c echo.Context) error {
+	id := c.Param("uuid")
+	c.Logger().Infof("Process::getAssessmentResult::uuid: %v", id)
 	assessmentResult := process.AssessmentResult{}
-	obj := s.Driver.Get(c.Request().Context(), assessmentResult.Type(), c.Param("uuid"), &assessmentResult)
 
-	if obj == nil {
-		return c.String(http.StatusNotFound, "object not found")
+	err := s.Driver.Get(c.Request().Context(), assessmentResult.Type(), id, &assessmentResult)
+
+	c.Logger().Infof("Process::getAssessmentResult::obj: %v", assessmentResult)
+	if err == nil {
+		return c.String(http.StatusNotFound, fmt.Errorf("object not found").Error())
 	}
 
-	return c.JSON(http.StatusOK, obj)
+	return c.JSON(http.StatusOK, assessmentResult)
 }
 
-func (s *Server) listAssessmentResults(c echo.Context) error {
+func (s *Server) getAssessmentResults(c echo.Context) error {
 	assessmentResult := process.AssessmentResult{}
 	objs, err := s.Driver.GetAll(c.Request().Context(), assessmentResult.Type(), &assessmentResult)
 
+	c.Logger().Infof("Process::getAssessmentResults::objs: %v", objs)
+
 	if err != nil {
-		return c.String(http.StatusInternalServerError, "failed to get object")
+		return c.String(http.StatusInternalServerError, fmt.Errorf("failed to get object: %v", err).Error())
 	}
 	return c.JSON(http.StatusOK, objs)
 }
