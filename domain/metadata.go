1d55e041d5ef7c46c478d3959b87963153a4eebf
Onsel Akin <onsela@outlook.com>
2023-10-22 15:12:09 +0300

    Removed ComprehensiveDetails struct

--- domain/metadata.go
+++ domain/metadata.go
@@ -1,6 +1,10 @@
 package domain
 
-import "time"
+import (
+	"strconv"
+	"strings"
+	"time"
+)
 
 type Metadata struct {
 	Revisions             []Revision `json:"revisions"`
@@ -23,10 +27,43 @@ type Revision struct {
 	OscalVersion string    `json:"oscalVersion"`
 }
 
-type ComprehensiveDetails struct {
-	Title       string     `json:"title,omitempty"`
-	Description string     `json:"description,omitempty"`
-	Props       []Property `json:"props,omitempty"`
-	Links       []Link     `json:"links,omitempty"`
-	Remarks     string     `json:"remarks,omitempty"`
+func NewRevision(title string, description string, remarks string) Revision {
+	revision := Revision{
+		Title:       title,
+		Description: description,
+		Remarks:     remarks,
+		Version:     "1.0.0",
+	}
+	return revision
+}
+
+func (r *Revision) bumpVersion(part int, title string, description string, remarks string) Revision {
+	versionParts := strings.Split(r.Version, ".")
+	versionPart, err := strconv.Atoi(versionParts[part])
+	if err != nil {
+		return Revision{}
+	}
+
+	versionPart++
+	versionParts[part] = strconv.Itoa(versionPart)
+	version := strings.Join(versionParts, ".")
+
+	return Revision{
+		Title:       title,
+		Description: description,
+		Remarks:     remarks,
+		Version:     version,
+	}
+}
+
+func (r *Revision) BumpMajor(title string, description string, remarks string) Revision {
+	return r.bumpVersion(0, title, description, remarks)
+}
+
+func (r *Revision) BumpMinor(title string, description string, remarks string) Revision {
+	return r.bumpVersion(1, title, description, remarks)
+}
+
+func (r *Revision) BumpPatch(title string, description string, remarks string) Revision {
+	return r.bumpVersion(2, title, description, remarks)
 }
