d825cf8adaa3907741d8837b0de65de567dfc99e
Adrienne Galloway <adrienne.galloway@engineerbetter.com>
2023-11-09 11:05:50 +0000

    creating a working DeleteCatalog function

--- store/catalog.go
+++ store/catalog.go
@@ -8,4 +8,5 @@ type CatalogStore interface {
 	CreateCatalog(catalog *domain.Catalog) (interface{}, error)
 	GetCatalog(id string) (*domain.Catalog, error)
 	UpdateCatalog(id string, catalog *domain.Catalog) error
+	DeleteCatalog(id string) error
 }
