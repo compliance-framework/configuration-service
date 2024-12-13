b19d28394d004620e05a58a407fcf2fc401f1051
Chris <chris@linguine.tech>
2024-12-13 14:34:04 +0100

    API test for create plan

--- api/handler/plan_api_test.go
+++ api/handler/plan_api_test.go
@@ -3,33 +3,51 @@
 package handler
 
 import (
-	"context"
-	"fmt"
-	mongo2 "github.com/compliance-framework/configuration-service/store/mongo"
+	"bytes"
+	"encoding/json"
+	"github.com/compliance-framework/configuration-service/event/bus"
+	"github.com/compliance-framework/configuration-service/service"
 	"github.com/compliance-framework/configuration-service/tests"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
-	"go.mongodb.org/mongo-driver/bson"
+	"go.uber.org/zap"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestPlanApi(t *testing.T) {
-
 	suite.Run(t, new(PlanIntegrationSuite))
-
 }
 
 type PlanIntegrationSuite struct {
 	tests.IntegrationTestSuite
 }
 
-func (suite *PlanIntegrationSuite) TestSomething() {
-	fmt.Println("Running a test")
+func (suite *PlanIntegrationSuite) TestCreatePlan() {
+	suite.Run("A plan can be created through the API", func() {
+		// Setup
+		e := echo.New()
+		reqBody, _ := json.Marshal(map[string]interface{}{
+			"title": "Some Plan",
+		})
 
-	suite.Run("Some test", func() {
-		ctx := context.Background()
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/plans",
+			bytes.NewReader(reqBody),
+		)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
 
-		mongo2.NewCatalogStore()
+		logger, _ := zap.NewProduction()
+		planHandler := NewPlanHandler(logger.Sugar(), service.NewPlanService(bus.Publish))
 
-		fmt.Println(mongo2.Collection("plan").Find(ctx, bson.D{}))
+		if assert.NoError(suite.T(), planHandler.CreatePlan(c)) {
+			assert.Equal(suite.T(), http.StatusCreated, rec.Code)
+			assert.NoError(suite.T(), json.Unmarshal(rec.Body.Bytes(), &idResponse{}))
+		}
 	})
 }
