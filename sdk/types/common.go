f1ea3100e20ac225b20c69411aa75d7d96e0d5da
Chris <chris@linguine.tech>
2025-03-14 15:19:42 +0100

    Go structs for results coming in from API requests

--- sdk/types/common.go
+++ sdk/types/common.go
@@ -17,3 +17,47 @@ type Property struct {
 	UUID    string `json:"uuid,omitempty" yaml:"uuid,omitempty"`
 	Value   string `json:"value" yaml:"value"`
 }
+
+type BackMatter struct {
+	Resources *[]Resource `json:"resources,omitempty" yaml:"resources,omitempty"`
+}
+
+type Base64 struct {
+	Filename  string `json:"filename,omitempty" yaml:"filename,omitempty"`
+	MediaType string `json:"media-type,omitempty" yaml:"media-type,omitempty"`
+	Value     string `json:"value" yaml:"value"`
+}
+
+type Citation struct {
+	Links *[]Link     `json:"links,omitempty" yaml:"links,omitempty"`
+	Props *[]Property `json:"props,omitempty" yaml:"props,omitempty"`
+	Text  string      `json:"text" yaml:"text"`
+}
+
+type ResourceLink struct {
+	Hashes    *[]Hash `json:"hashes,omitempty" yaml:"hashes,omitempty"`
+	Href      string  `json:"href" yaml:"href"`
+	MediaType string  `json:"media-type,omitempty" yaml:"media-type,omitempty"`
+}
+
+type DocumentId struct {
+	Identifier string `json:"identifier" yaml:"identifier"`
+	Scheme     string `json:"scheme,omitempty" yaml:"scheme,omitempty"`
+}
+
+type Hash struct {
+	Algorithm string `json:"algorithm" yaml:"algorithm"`
+	Value     string `json:"value" yaml:"value"`
+}
+
+type Resource struct {
+	Base64      *Base64         `json:"base64,omitempty" yaml:"base64,omitempty"`
+	Citation    *Citation       `json:"citation,omitempty" yaml:"citation,omitempty"`
+	Description string          `json:"description,omitempty" yaml:"description,omitempty"`
+	DocumentIds *[]DocumentId   `json:"document-ids,omitempty" yaml:"document-ids,omitempty"`
+	Props       *[]Property     `json:"props,omitempty" yaml:"props,omitempty"`
+	Remarks     string          `json:"remarks,omitempty" yaml:"remarks,omitempty"`
+	Rlinks      *[]ResourceLink `json:"rlinks,omitempty" yaml:"rlinks,omitempty"`
+	Title       string          `json:"title,omitempty" yaml:"title,omitempty"`
+	UUID        string          `json:"uuid" yaml:"uuid"`
+}
