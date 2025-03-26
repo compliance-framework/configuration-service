e3a27e7cf38080b7396eb14e8ae35f9a8dd405bd
Chris <chris@linguine.tech>
2025-03-26 12:00:11 +0100

    Generate docs

--- docs/docs.go
+++ docs/docs.go
@@ -135,6 +135,41 @@ const docTemplate = `{
                 }
             }
         },
+        "/compliance-by-uuid/{uuid}": {
+            "get": {
+                "description": "Fetches an intervalled compliance report for findings that match the provided uuid. The report groups findings status over time and returns a list of compliance report groups.",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "Findings"
+                ],
+                "summary": "Get intervalled compliance report by finding uuid",
+                "responses": {
+                    "201": {
+                        "description": "Created",
+                        "schema": {
+                            "$ref": "#/definitions/handler.GenericDataListResponse-service_StatusOverTimeGroup"
+                        }
+                    },
+                    "422": {
+                        "description": "Unprocessable Entity",
+                        "schema": {
+                            "$ref": "#/definitions/api.Error"
+                        }
+                    },
+                    "500": {
+                        "description": "Internal Server Error",
+                        "schema": {
+                            "$ref": "#/definitions/api.Error"
+                        }
+                    }
+                }
+            }
+        },
         "/findings": {
             "post": {
                 "description": "Creates multiple findings in the CCF API, as well as their subject and component counterparts.",
@@ -167,9 +202,9 @@ const docTemplate = `{
                 }
             }
         },
-        "/findings/compliance": {
+        "/findings/compliance-by-search": {
             "post": {
-                "description": "Fetches an intervalled compliance report for findings that match the provided uuid. The report groups findings status over time and returns a list of compliance report groups.",
+                "description": "Fetches an intervalled compliance report for findings that match the provided label filter. The report groups findings status over time and returns a list of compliance report groups.",
                 "consumes": [
                     "application/json"
                 ],
@@ -179,7 +214,18 @@ const docTemplate = `{
                 "tags": [
                     "Findings"
                 ],
-                "summary": "Get intervalled compliance report by finding uuid",
+                "summary": "Get intervalled compliance report by search",
+                "parameters": [
+                    {
+                        "description": "Label filter criteria",
+                        "name": "filter",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/labelfilter.Filter"
+                        }
+                    }
+                ],
                 "responses": {
                     "201": {
                         "description": "Created",
