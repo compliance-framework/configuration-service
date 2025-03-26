8c036335dd3e2672b53d5e3eecf93e0194652f5f
Harry Lincoln <harry.lincoln@container-solutions.com>
2025-03-26 12:21:39 +0000

    UpdateSubjectById

--- internal/api/handler/subjects.go
+++ internal/api/handler/subjects.go
@@ -7,6 +7,7 @@ import (
 	"github.com/compliance-framework/configuration-service/internal/service"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"go.uber.org/zap"
 )
@@ -25,6 +26,7 @@ func NewSubjectsHandler(l *zap.SugaredLogger, s *service.SubjectService) *Subjec
 
 func (h *SubjectsHandler) Register(api *echo.Group) {
 	api.GET("/:id", h.FindSubjectById)
+	api.PATCH("/:id", h.UpdateSubjectById)
 }
 
 // FindSubjectById godoc
@@ -55,5 +57,50 @@ func (h *SubjectsHandler) FindSubjectById(ctx echo.Context) error {
 
 	return ctx.JSON(http.StatusOK, GenericDataResponse[service.Subject]{
 		Data: *subject,
+	})
+}
+
+// UpdateSubjectById godoc
+//
+//	@Summary		Update a subject's title and/or remarks
+//	@Description	Updates a subject's title and/or remarks based on the provided subject ID. Only title and remarks are updated if provided. If no fields are provided, a `400 Bad Request` is returned.
+//	@Tags			Subjects
+//	@Produce		json
+//	@Param			subjectId	path		string	true	"Subject ID"
+//	@Param			body		body		models.Subject	true	"Subject data"
+//	@Success		200	{object}	models.Subject
+//	@Failure		400	{object}	api.Error
+//	@Failure		404	{object}	api.Error
+//	@Failure		500	{object}	api.Error
+//	@Router			/subjects/{subjectId} [patch]
+func (h *SubjectsHandler) UpdateSubjectById(ctx echo.Context) error {
+
+	subjectID := ctx.Param("subjectId")
+	id, err := uuid.Parse(subjectID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid subject ID format")
+	}
+
+	var updatedSubject service.Subject
+	if err := ctx.Bind(&updatedSubject); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid input data")
+	}
+
+	// Check if title or remarks are present and update them
+	// If no changes to title or remarks, the fields will stay the same
+	if updatedSubject.Title == "" && updatedSubject.Remarks == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "No title or remarks to update")
+	}
+
+	updated, err := h.service.Update(ctx.Request().Context(), &id, &updatedSubject)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return echo.NewHTTPError(http.StatusNotFound, "Subject not found")
+		}
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to update subject")
+	}
+
+	return ctx.JSON(http.StatusOK, GenericDataResponse[service.Subject]{
+		Data: *updated,
 	})
 }
