8a4ac9e80f6f6230e51ebae9295ca8cf3bd412d9
Sam Sherar <sam.sherar@container-solutions.com>
2025-06-11 14:36:16 +0100

    (core) swagger comment fmt

--- main.go
+++ main.go
@@ -8,17 +8,17 @@ import (
 
 
 // Swagger documentation
-//	@title						Continuous Compliance Framework API
-//	@version					1
-//	@description				This is the API for the Continuous Compliance Framework.
-//	@host						localhost:8080
-//	@accept						json
-//	@produce					json
-//	@BasePath					/api
-//	@externalDocs.description	OpenAPI
-//	@externalDocs.url			https://swagger.io/resources/open-api/
-//  @securitydefinitions.oauth2.password	OAuth2Password
-//  @tokenUrl								/api/auth/token
+//	@title									Continuous Compliance Framework API
+//	@version								1
+//	@description							This is the API for the Continuous Compliance Framework.
+//	@host									localhost:8080
+//	@accept									json
+//	@produce								json
+//	@BasePath								/api
+//	@externalDocs.description				OpenAPI
+//	@externalDocs.url						https://swagger.io/resources/open-api/
+//	@securitydefinitions.oauth2.password	OAuth2Password
+//	@tokenUrl								/api/auth/token
 func main() {
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
