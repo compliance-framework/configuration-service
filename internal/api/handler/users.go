ea9862fe7b3cdd6ac6f8d2c0b3e10090fee4d62b
Sam Sherar <sam.sherar@container-solutions.com>
2025-07-18 13:52:15 +0100

    Add user API stubs

---
+++ internal/api/handler/users.go
@@ -0,0 +1,70 @@
+package handler
+
+import (
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type UserHandler struct {
+	sugar *zap.SugaredLogger
+	db    *gorm.DB
+}
+
+func NewUserHandler(sugar *zap.SugaredLogger, db *gorm.DB) *UserHandler {
+	return &UserHandler{
+		sugar: sugar,
+		db:    db,
+	}
+}
+
+func (h *UserHandler) Register(api *echo.Group) {
+	api.GET("", h.ListUsers)
+	api.GET("/:id", h.GetUser)
+	api.POST("", h.CreateUser)
+	api.PUT("/:id", h.UpdateUser)
+	api.DELETE("/:id", h.DeleteUser)
+	api.POST("/me/change-password", h.ChangeLoggedInUserPassword)
+	api.POST("/:id/change-password", h.ChangePassword)
+	api.GET("/me", h.GetMe)
+}
+
+func (h *UserHandler) ListUsers(ctx echo.Context) error {
+	// This method will be implemented later to list users.
+	return ctx.JSON(501, "Not Implemented")
+}
+
+func (h *UserHandler) GetUser(ctx echo.Context) error {
+	// This method will be implemented later to get a specific user.
+	return ctx.JSON(501, "Not Implemented")
+}
+
+func (h *UserHandler) GetMe(ctx echo.Context) error {
+	// This method will be implemented later to get the current user's information.
+	return ctx.JSON(501, "Not Implemented")
+}
+
+func (h *UserHandler) CreateUser(ctx echo.Context) error {
+	// This method will be implemented later to create a new user.
+	return ctx.JSON(501, "Not Implemented")
+}
+
+func (h *UserHandler) UpdateUser(ctx echo.Context) error {
+	// This method will be implemented later to update an existing user.
+	return ctx.JSON(501, "Not Implemented")
+}
+
+func (h *UserHandler) DeleteUser(ctx echo.Context) error {
+	// This method will be implemented later to delete a user.
+	return ctx.JSON(501, "Not Implemented")
+}
+
+func (h *UserHandler) ChangeLoggedInUserPassword(ctx echo.Context) error {
+	// This method will be implemented later to change a user's password.
+	return ctx.JSON(501, "Not Implemented")
+}
+
+func (h *UserHandler) ChangePassword(ctx echo.Context) error {
+	// This method will be implemented later to change a user's password.
+	return ctx.JSON(501, "Not Implemented")
+}
